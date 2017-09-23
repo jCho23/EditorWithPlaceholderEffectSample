a0a7dec1dc29ec0556bd1ef4235ff87af03891f5
June Cho <junecho.ucb@gmail.com>
2017-09-22 19:43:29 -0700

    CleanUp

---
+++ Pages/EditorWithPlaceholderEffectPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace EditorWithPlaceholderEffectSample.Pages
+{
+    public partial class EditorWithPlaceholderEffectPage : ContentPage
+    {
+        public EditorWithPlaceholderEffectPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
